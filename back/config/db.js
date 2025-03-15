f397d969663cf7218d8ba8e895062eb2955f4305
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-03-15 15:55:10 -0300

    Removendo a pasta back

--- back/config/db.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
-
-const url = process.env.URL;
-const apikey = process.env.APIKEY;
-
-const supabase = createClient(url, apikey);
-
-module.exports = supabase;
