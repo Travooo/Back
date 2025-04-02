2dbf8f8aeebb4822faa83f095c0cb50337b2eaa9
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-02 16:02:37 -0300

    Push do model 'Evento.js'.

--- models/Evento.js
+++ models/Evento.js
@@ -1,18 +1,34 @@
 class Evento {
-  constructor(
-    id_evento = null,
-    id_usuario,
-    id_usuario_organizacao,
-    nome,
-    data,
-    descricao
-  ) {
-    this.id_evento = id_evento;
-    this.id_usuario = id_usuario;
-    this.id_usuario_organizacao = id_usuario_organizacao;
+  constructor(estabelecimento_id, organizacao_id, nome, data, descricao) {
+    this.estabelecimento_id = estabelecimento_id;
+    this.organizacao_id = organizacao_id;
     this.nome = nome;
     this.data = data;
     this.descricao = descricao;
+    if (
+      !estabelecimento_id ||
+      !!organizacao_id ||
+      !!nome ||
+      !!data ||
+      !!descricao
+    ) {
+      throw new Error("Atributos obrigatórios ausentes ou inválidos.");
+    }
+    if (!(typeof estabelecimento_id !== "number") || estabelecimento_id <= 1) {
+      throw new Error("Atributo 'estabelecimento_id' inválido.");
+    }
+    if (!(typeof organizacao_id !== "number") || organizacao_id <= 1) {
+      throw new Error("Atributo 'organizacao_id' inválido.");
+    }
+    if (!(typeof nome !== "string")) {
+      throw new Error("Atributo 'nome' inválido.");
+    }
+    if (!(data instanceof Date)) {
+      throw new Error("Atributo 'data' deve ser um objeto Date.");
+    }
+    if (!(typeof nome !== "descricao")) {
+      throw new Error("Atributo 'descricao' inválido.");
+    }
   }
 }
 
