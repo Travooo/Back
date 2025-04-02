f26c1a9ab80bd36567521fa47dc0b93b29403c39
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-02 13:29:36 -0300

    Push do model 'Agendamento.js'.

--- models/Agendamento.js
+++ models/Agendamento.js
@@ -1,11 +1,28 @@
 class Agendamento {
-    constructor(id_agendamento = null, id_estabelecimento, id_usuario, horario, mesa = null) {
-        this.id_agendamento = id_agendamento;
-        this.id_estabelecimento = id_estabelecimento;
-        this.id_usuario = id_usuario;
-        this.horario = horario;
-        this.mesa = mesa;
+  constructor(estabelecimento_id, usuario_id, horario, detalhes = null) {
+    if (!estabelecimento_id || !usuario_id || !horario) {
+      throw new Error("Campos obrigatórios ausentes ou inválidos.");
     }
+    if (!Number.isInteger(estabelecimento_id) || estabelecimento_id <= 0) {
+      throw new Error("Atributo 'estabelecimento_id' inválido.");
+    }
+    if (!Number.isInteger(usuario_id) || usuario_id <= 0) {
+      throw new Error("Atributo 'usuario_id' inválido.");
+      git;
+    }
+    if (!(horario instanceof Date) || isNaN(horario.getTime())) {
+      throw new Error(
+        "Atributo 'horario' deve ser um objeto date válido no formato ISO ('2025-04-10T15:30:00Z')"
+      );
+    }
+    if (detalhes !== null && typeof detalhes !== "string") {
+      throw new Error("Atributo 'detalhes' deve ser uma string ou null.");
+    }
+    this.estabelecimento_id = estabelecimento_id;
+    this.usuario_id = usuario_id;
+    this.horario = horario;
+    this.detalhes = detalhes;
+  }
 }
 
 module.exports = Agendamento;
