222c920f505eafaab59aea1cd543519a2ed4f1fb
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-02 15:16:58 -0300

    Push do model 'Cupom.js'.

--- models/Cupom.js
+++ models/Cupom.js
@@ -1,8 +1,19 @@
 class Cupom {
-  constructor(id_cupom = null, id_estabelecimento, id_usuario, descricao) {
-    this.id_cupom = id_cupom;
-    this.id_estabelecimento = id_estabelecimento;
-    this.id_usuario = id_usuario;
+  constructor(estabelecimento_id, usuario_id, descricao) {
+    if (!estabelecimento_id || !usuario_id || !descricao) {
+      throw new Erros("Campos obrigatórios ausentes ou inválidos.");
+    }
+    if (typeof estabelecimento_id !== "number") {
+      throw new Error("Atributo 'estabelecimento_id' inválido.");
+    }
+    if (typeof usuario_id !== "number") {
+      throw new Error("Atributo 'usuario_id' inválido.");
+    }
+    if (typeof descricao !== "string") {
+      throw new Error("Atributo 'descricao' deve ser uma string.");
+    }
+    this.estabelecimento_id = estabelecimento_id;
+    this.usuario_id = usuario_id;
     this.descricao = descricao;
   }
 }
