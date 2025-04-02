54542078bb51cfc555c0df749596ae44c4d3fc5b
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-02 15:40:49 -0300

    Push do model 'Estabelecimento.js'.

--- models/Estabelecimento.js
+++ models/Estabelecimento.js
@@ -1,13 +1,35 @@
 class Estabelecimento {
-  constructor(
-    id_estabelecimento = null,
-    nome,
-    sobre,
-    endereco,
-    foto_local,
-    horarios
-  ) {
-    this.id_estabelecimento = id_estabelecimento;
+  constructor(organizacao_id, nome, sobre, endereco, foto_local, horarios) {
+    if (
+      !organizao_id ||
+      !nome ||
+      !sobre ||
+      !endereco ||
+      !foto_local ||
+      !horarios
+    ) {
+      throw new Error("Campos obrigatórios ausentes ou inválidos.");
+    }
+    if (!Number.isInteger(organizacao_id) || organizacao_id <= 0) {
+      throw new Error("Atributo 'organizao_id' inválido.");
+    }
+    if (typeof nome !== "string") {
+      throw new Error("Atributo 'nome' inválido.");
+    }
+    if (typeof sobre !== "string") {
+      throw new Error("Atributo 'sobre' inválido.");
+    }
+    if (typeof endereco !== "string") {
+      throw new Error("Atributo 'sobre' inválido.");
+    }
+    if (!(foto_local instanceof Buffer)) {
+      throw new Error("Atributo 'foto_local' inválido.");
+    }
+    if (typeof horarios !== "string") {
+      throw new Error("Atributo 'horarios' inválido.");
+    }
+
+    this.organizacao_id = organizacao_id;
     this.nome = nome;
     this.sobre = sobre;
     this.endereco = endereco;
