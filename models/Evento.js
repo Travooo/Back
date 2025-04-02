174236f14f6446149d1ed852825e98633deb7530
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-02 16:35:30 -0300

    Correção do model 'evento.js'.

--- models/Evento.js
+++ models/Evento.js
@@ -14,19 +14,19 @@ class Evento {
     ) {
       throw new Error("Atributos obrigatórios ausentes ou inválidos.");
     }
-    if (!(typeof estabelecimento_id !== "number") || estabelecimento_id <= 1) {
+    if (typeof estabelecimento_id !== "number" || estabelecimento_id <= 1) {
       throw new Error("Atributo 'estabelecimento_id' inválido.");
     }
-    if (!(typeof organizacao_id !== "number") || organizacao_id <= 1) {
+    if (typeof organizacao_id !== "number" || organizacao_id <= 1) {
       throw new Error("Atributo 'organizacao_id' inválido.");
     }
-    if (!(typeof nome !== "string")) {
+    if (typeof nome !== "string") {
       throw new Error("Atributo 'nome' inválido.");
     }
-    if (!(data instanceof Date)) {
+    if (data instanceof Date) {
       throw new Error("Atributo 'data' deve ser um objeto Date.");
     }
-    if (!(typeof nome !== "descricao")) {
+    if (typeof nome !== "descricao") {
       throw new Error("Atributo 'descricao' inválido.");
     }
   }
