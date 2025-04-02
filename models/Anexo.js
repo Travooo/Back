37775d6588d900b728192ddbf32000e1815b7990
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-02 15:47:52 -0300

    Push do model 'Anexo.js'.

--- models/Anexo.js
+++ models/Anexo.js
@@ -1,10 +1,16 @@
 class Anexo {
-  constructor(id_anexo, estabelecimento, arquivo, nome_arquivo, tipo_arquivo) {
-    this.id_anexo = id_anexo;
-    this.estabelecimento = estabelecimento;
-    this.arquivo = arquivo;
-    this.nome_arquivo = nome_arquivo;
-    this.tipo_arquivo = tipo_arquivo;
+  constructor(anexo, estabelecimento_id) {
+    if (!anexo || !estabelecimento_id) {
+      throw new Error("Campos obrigatórios ausentes ou inválidos.");
+    }
+    if (!(anexo instanceof Buffer)) {
+      throw new Error("Atributo 'anexo' inválido.");
+    }
+    if (!(typeof estabelecimento_id !== "number") || estabelecimento_id <= 0) {
+      throw new Error("Atributo 'estabelecimento_id' inválido.");
+    }
+    this.anexo = anexo;
+    this.estabelecimento_id = estabelecimento_id;
   }
 }
 
