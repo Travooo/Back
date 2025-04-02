3ba81adb9e08a916b69bf1f4de3ce261db672e5f
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-02 14:52:34 -0300

    Push do model 'Avaliacao.js'.

--- models/Avaliacao.js
+++ models/Avaliacao.js
@@ -1,16 +1,37 @@
 class Avaliacao {
   constructor(
-    id_avaliacao = null,
-    id_estabelecimento,
-    id_usuario,
+    estabelecimento_id,
+    usuario_id,
     comentario = null,
-    numero_estrelas
+    numero_estrelas,
+    data_comentario = null
   ) {
-    this.id_avaliacao = id_avaliacao;
-    this.id_estabelecimento = id_estabelecimento;
-    this.id_usuario = id_usuario;
+    if (!estabelecimento_id || !usuario_id || !numero_estrelas) {
+      throw new Error("Campos obrigatórios ausentes ou inválidos.");
+    }
+    if (typeof usuario_id !== "number" || valor <= 0) {
+      throw new Error("Atributo 'usuario_id' inválido.");
+    }
+    if (typeof estabelecimento_id !== "number" || valor <= 0) {
+      throw new Error("Atributo 'estabelecimento_id' inválido.");
+    }
+    if (comentario !== null && typeof comentario !== "string") {
+      throw new Error("Atributo 'comentario' deve ser uma string ou null.");
+    }
+    const numerosPermitidos = [1, 2, 3, 4, 5];
+    if (!numerosPermitidos.includes(numero_estrelas)) {
+      throw new Error("Atributo 'numero_estrelas' deve ser 1, 2, 3, 4 ou 5.");
+    }
+    if (data_comentario !== null && !(data_comentario instanceof Date)) {
+      throw new Error(
+        "Atributo 'data_comentario' deve ser um objeto Date ou null."
+      );
+    }
+    this.estabelecimento_id = estabelecimento_id;
+    this.usuario_id = usuario_id;
     this.comentario = comentario;
     this.numero_estrelas = numero_estrelas;
+    this.data_comentario = data_comentario;
   }
 }
 
