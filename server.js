aa96dc2f3ee8166d0b231a2c445682b8e61175f4
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-03 18:58:43 -0300

    Descomentando definição dos caminhos dos routers e definição das rotas em 'Server.js'.

--- server.js
+++ server.js
@@ -13,18 +13,18 @@ const supabase = createClient(
 );
 
 // Importação das rotas
-//const caminho_agendamento_router = require("./routes/agendamentoRouter");
-//const caminho_anexo_router = require("./routes/anexoRouter");
-//const caminho_avaliacao_router = require("./routes/avaliacaoRouter");
-//const caminho_conexao_router = require("./routes/conexaoRouter");
-//const caminho_cupom_router = require("./routes/cupomRouter");
-//const caminho_estabelecimento_router = require("./routes/estabelecimentoRouter");
-//const caminho_evento_router = require("./routes/eventoRouter");
-//const caminho_favorito_router = require("./routes/favoritoRouter");
-//const caminho_local_visitado_router = require("./routes/localVisitadoRouter");
-//const caminho_pagamento_router = require("./routes/pagamentoRouter");
+caminho_agendamento_router = require("./routes/agendamentoRouter");
+caminho_anexo_router = require("./routes/anexoRouter");
+caminho_avaliacao_router = require("./routes/avaliacaoRouter");
+caminho_conexao_router = require("./routes/conexaoRouter");
+caminho_cupom_router = require("./routes/cupomRouter");
+caminho_estabelecimento_router = require("./routes/estabelecimentoRouter");
+caminho_evento_router = require("./routes/eventoRouter");
+caminho_favorito_router = require("./routes/favoritoRouter");
+caminho_local_visitado_router = require("./routes/localVisitadoRouter");
+caminho_pagamento_router = require("./routes/pagamentoRouter");
 const caminho_usuario_router = require("./routes/usuarioRouter");
-//const caminho_organizacao_router = require("./routes/organizacaoRouter");
+const caminho_organizacao_router = require("./routes/organizacaoRouter");
 
 // Middlewares:
 app.use(cors());
@@ -47,17 +47,17 @@ app.get("/test-supabase", async (req, res) => {
 });
 
 // Usar as rotas
-//app.use("/agendamentos", caminho_agendamento_router);
-//app.use("/anexos", caminho_anexo_router);
-//app.use("/avaliacoes", caminho_avaliacao_router);
-//app.use("/conexoes", caminho_conexao_router);
-//app.use("/cupons", caminho_cupom_router);
-////app.use("/estabelecimentos", caminho_estabelecimento_router);
-//app.use("/eventos", caminho_evento_router);
-//app.use("/favoritos", caminho_favorito_router);
-//app.use("/locais_visitados", caminho_local_visitado_router);
-//app.use("/organizacoes", caminho_organizacao_router);
-//app.use("/pagamentos", caminho_pagamento_router);
+app.use("/agendamentos", caminho_agendamento_router);
+app.use("/anexos", caminho_anexo_router);
+app.use("/avaliacoes", caminho_avaliacao_router);
+app.use("/conexoes", caminho_conexao_router);
+app.use("/cupons", caminho_cupom_router);
+app.use("/estabelecimentos", caminho_estabelecimento_router);
+app.use("/eventos", caminho_evento_router);
+app.use("/favoritos", caminho_favorito_router);
+app.use("/locais_visitados", caminho_local_visitado_router);
+app.use("/organizacoes", caminho_organizacao_router);
+app.use("/pagamentos", caminho_pagamento_router);
 app.use("/usuarios", caminho_usuario_router);
 
 if (require.main === module) {
