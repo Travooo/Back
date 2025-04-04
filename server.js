7013e607dae042661146597d72cacbd680d4370b
L3VILL1 <145598982+L3VILL1@users.noreply.github.com>
2025-04-04 19:56:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
+// ROTAS
 const userRouteV1 = require('./routes/rota_usuario');
 const usuarioOrgRouteV1 = require('./routes/rota_usuario_organizacao');
+const conexaoRouteV1 = require('./routes/rota_conexao');
+
 
 const port = process.env.PORT || 3000;
 
@@ -12,6 +16,7 @@ app.use(express.json());
 
 app.use('/rest/v1', userRouteV1);
 app.use('/rest/v1', usuarioOrgRouteV1);
+app.use('/rest/v1', conexaoRouteV1);
 
 app.listen(port, () => {
     console.log(`✅ Servidor rodando na porta ${port}`);
