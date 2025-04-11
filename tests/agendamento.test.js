2f2c42fca9cd9a94967ec8f918824e8738b8b26b
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-11 19:35:31 -0300

    tests: novo arquivo de testes do usuário

--- tests/agendamento.test.js
+++ tests/agendamento.test.js
@@ -1,7 +1,7 @@
-const request = require('supertest');
-const app = require('../app');
+const request = require('supertest')
+const { app } = require('../app')
 
-let agendamentoCriadoId;
+let agendamentoCriadoId
 
 describe('Testes de Integração - Agendamento', () => {
   test('Deve criar um agendamento via API', async () => {
@@ -10,43 +10,43 @@ describe('Testes de Integração - Agendamento', () => {
       usuario_id: '1',
       horario: '2025-04-06T14:30:00Z',
       detalhes: 'Usuário Teste API',
-    };
-    const res = await request(app).post('/agendamentos').send(new_agendamento);
-    console.log('Resposta da criação:', res.body);
-    expect(res.status).toBe(201);
-    expect(res.body).toHaveProperty('id');
-    agendamentoCriadoId = res.body.id;
-  });
+    }
+    const res = await request(app).post('/agendamentos').send(new_agendamento)
+    console.log('Resposta da criação:', res.body)
+    expect(res.status).toBe(201)
+    expect(res.body).toHaveProperty('id')
+    agendamentoCriadoId = res.body.id
+  })
 
   test('Deve listar um agendamento do Supabase', async () => {
-    const res = await request(app).get(`/agendamentos/${agendamentoCriadoId}`);
-    console.log('Agendamento buscado:', res.body);
-    expect(res.status).toBe(200);
-    expect(res.body).toBeDefined();
-    expect(res.body).toHaveProperty('id', agendamentoCriadoId);
-  });
+    const res = await request(app).get(`/agendamentos/${agendamentoCriadoId}`)
+    console.log('Agendamento buscado:', res.body)
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveProperty('id', agendamentoCriadoId)
+  })
 
   test('Deve listar agendamentos do Supabase', async () => {
-    const res = await request(app).get('/agendamentos');
-    console.log('Total de agendamentos:', res.body.length);
-    expect(res.status).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
-    expect(res.body.length).toBeGreaterThan(0);
-  });
+    const res = await request(app).get('/agendamentos')
+    console.log('Total de agendamentos:', res.body.length)
+    expect(res.status).toBe(200)
+    expect(Array.isArray(res.body)).toBe(true)
+    expect(res.body.length).toBeGreaterThan(0)
+  })
 
   test('Deve atualizar um agendamento existente', async () => {
-    const updates = { horario: '2025-04-06T15:40:00Z' };
-    const res = await request(app).put(`/agendamentos/${agendamentoCriadoId}`).send(updates);
-    console.log('Agendamento atualizado:', res.body);
-    expect(res.status).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
-    expect(res.body[0].horario.startsWith('2025-04-06T15:40:00')).toBe(true);
-  });
+    const updates = { horario: '2025-04-06T15:40:00Z' }
+    const res = await request(app).put(`/agendamentos/${agendamentoCriadoId}`).send(updates)
+    console.log('Agendamento atualizado:', res.body)
+    expect(res.status).toBe(200)
+    expect(Array.isArray(res.body)).toBe(true)
+    expect(res.body[0].horario.startsWith('2025-04-06T15:40:00')).toBe(true)
+  })
 
   test('Deve remover um agendamento do Supabase', async () => {
-    const res = await request(app).delete(`/agendamentos/${agendamentoCriadoId}`);
-    console.log('Resposta da exclusão:', res.body);
-    expect(res.status).toBe(204);
-    expect(res.body).not.toBeInstanceOf(Error);
-  });
-});
+    const res = await request(app).delete(`/agendamentos/${agendamentoCriadoId}`)
+    console.log('Resposta da exclusão:', res.body)
+    expect(res.status).toBe(204)
+    expect(res.body).not.toBeInstanceOf(Error)
+  })
+})
--- tests/usuario.test.js
+++ tests/usuario.test.js
@@ -1,58 +1,73 @@
-require('dotenv').config();
-const request = require('supertest');
-const app = require('../app');
+require('dotenv').config()
+const app = require('../src/app')
+const request = require('supertest')
 
-let usuarioCriadoId;
+let usuarioCriadoId
 
 describe('Testes de Integração - Usuario', () => {
+  const new_user = {
+    email: 'new_email@email.com',
+    senha: 'senha123',
+    nome_usuario: 'testeUser',
+    nome_completo: 'Usuário Teste API',
+    foto_perfil:
+      'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIHWP4//8/AwAI/AL+fctJ7wAAAABJRU5ErkJggg==',
+    data_nascimento: '2002-06-20',
+    admin: false,
+    tipo_plano: 1,
+  }
   test('Deve criar um usuário via API', async () => {
-    const new_user = {
-      email: 'usuario.teste@email.com',
-      senha: 'senha123',
-      nome_usuario: 'testeUser',
-      nome_completo: 'Usuário Teste API',
-      foto_perfil:
-        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIHWP4//8/AwAI/AL+fctJ7wAAAABJRU5ErkJggg==',
-      data_nascimento: '2002-06-20',
-      admin: false,
-      tipo_plano: 1,
-    };
-    const res = await request(app).post('/usuarios').send(new_user);
-    console.log('Resposta da criação:', res.body);
-    expect(res.status).toBe(201);
-    expect(res.body).toHaveProperty('id');
-    usuarioCriadoId = res.body.id;
-  });
+    const res = await request(app).post('/usuarios').send(new_user)
+    console.log('Resposta da criação:', res.body)
+    expect(res.status).toBe(201)
+    expect(res.body).toHaveProperty('id')
+    usuarioCriadoId = res.body.id
+    console.log('Resposta da criação:', res.body)
+    console.log('Usuário buscado:', res.body)
+    console.log('Total de usuários:', res.body.length)
+  })
 
   test('Deve listar um usuário do Supabase', async () => {
-    const res = await request(app).get(`/usuarios/${usuarioCriadoId}`);
-    console.log('Usuário buscado:', res.body);
-    expect(res.status).toBe(200);
-    expect(res.body).toBeDefined();
-    expect(res.body).toHaveProperty('id', usuarioCriadoId);
-  });
+    const res = await request(app).get(`/usuarios/${usuarioCriadoId}`)
+    console.log('Usuário buscado:', res.body)
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveProperty('id', usuarioCriadoId)
+    expect(res.body).not.toHaveProperty('senha')
+    console.log('Resposta da criação:', res.body)
+    console.log('Usuário buscado:', res.body)
+    console.log('Total de usuários:', res.body.length)
+  })
 
   test('Deve listar usuários do Supabase', async () => {
-    const res = await request(app).get('/usuarios');
-    console.log('Total de usuários:', res.body.length);
-    expect(res.status).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
-    expect(res.body.length).toBeGreaterThan(0);
-  });
+    const res = await request(app).get('/usuarios')
+    console.log('Total de usuários:', res.body.length)
+    expect(res.status).toBe(200)
+    expect(Array.isArray(res.body)).toBe(true)
+    expect(res.body.length).toBeGreaterThan(0)
+    console.log('Resposta da criação:', res.body)
+    console.log('Usuário buscado:', res.body)
+    console.log('Total de usuários:', res.body.length)
+  })
 
   test('Deve atualizar um usuario existente', async () => {
-    const updates = { nome_usuario: 'NomeAtualizadoViaTeste' };
-    const res = await request(app).put(`/usuarios/${usuarioCriadoId}`).send(updates);
-    console.log('Usuário atualizado:', res.body);
-    expect(res.status).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
-    expect(res.body[0].nome_usuario).toBe('NomeAtualizadoViaTeste');
-  });
+    const updates = { nome_usuario: 'NomeAtualizadoViaTeste' }
+    const res = await request(app).put(`/usuarios/${usuarioCriadoId}`).send(updates)
+    console.log('Usuário atualizado:', res.body)
+    expect(res.status).toBe(200)
+    expect(Array.isArray(res.body)).toBe(true)
+    expect(res.body[0].nome_usuario).toBe('NomeAtualizadoViaTeste')
+    console.log('Resposta da criação:', res.body)
+    console.log('Usuário buscado:', res.body)
+    console.log('Total de usuários:', res.body.length)
+  })
 
   test('Deve remover um usuário do Supabase', async () => {
-    const res = await request(app).delete(`/usuarios/${usuarioCriadoId}`);
-    console.log('Resposta da exclusão:', res.body);
-    expect(res.status).toBe(204);
-    expect(res.body).toEqual({});
-  });
-});
+    const res = await request(app).delete(`/usuarios/${usuarioCriadoId}`)
+    console.log('Resposta da exclusão:', res.body)
+    expect(res.status).toBe(204)
+    console.log('Resposta da criação:', res.body)
+    console.log('Usuário buscado:', res.body)
+    console.log('Total de usuários:', res.body.length)
+  })
+})
