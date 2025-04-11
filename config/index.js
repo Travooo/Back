3026fc570dfe94702ac99bf7f325eefff5afd307
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-11 14:28:23 -0300

    refactor(index.js): limitar a inicialização do servidor

--- config/index.js
+++ config/index.js
@@ -1,17 +1,8 @@
 require('dotenv').config();
-
-const { SUPABASE_URL, SUPABASE_KEY } = process.env;
-if (!SUPABASE_URL || !SUPABASE_KEY) {
-  throw new Error('Variáveis de ambiente do Supabase estão faltando. Cheque seu .env.');
-}
-
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}.`);
 });
-
-module.exports = supabase;
