834d85d6fed67edd9034f33bc3ab29c60420e92f
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-11 19:20:41 -0300

    refactor: modularizar estrutura app.js, supabaseClient.js e inex.js

--- src/app.js
+++ src/app.js
@@ -1,47 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const { createClient } = require('@supabase/supabase-js');
+const express = require('express')
+const cors = require('cors')
+const supabase = require('./config/supabaseClient')
 
-const { SUPABASE_URL, SUPABASE_KEY } = process.env;
-if (!SUPABASE_URL || !SUPABASE_KEY) {
-  throw new Error('Variáveis de ambiente do Supabase estão faltando. Cheque seu .env.');
-}
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 // Rota teste de conexão Supabase:
 app.get('/test-supabase', async (req, res) => {
   try {
-    const { data, error } = await supabase.from('usuario').select('*').limit(10);
-    if (error) throw error;
+    const { data, error } = await supabase.from('usuario').select('*').limit(10)
+    if (error) throw error
 
-    res.json({ success: true, data });
+    res.json({ success: true, data })
   } catch (error) {
-    res.status(500).json({ success: false, message: 'Erro ao conectar ao Supabase. ', error });
+    res.status(500).json({ success: false, message: 'Erro ao conectar ao Supabase. ', error })
   }
-});
+})
 
 const rotas = [
-  ['agendamentos', 'agendamentoRouter'],
-  ['anexos', 'anexoRouter'],
-  ['avaliacoes', 'avaliacaoRouter'],
-  ['conexoes', 'conexaoRouter'],
-  ['cupons', 'cupomRouter'],
-  ['estabelecimentos', 'estabelecimentoRouter'],
-  ['eventos', 'eventoRouter'],
-  ['favoritos', 'favoritoRouter'],
-  ['locais_visitados', 'localVisitadoRouter'],
-  ['organizacoes', 'organizacaoRouter'],
-  ['pagamentos', 'pagamentoRouter'],
+  //['agendamentos', 'agendamentoRouter'],
+  //['anexos', 'anexoRouter'],
+  //['avaliacoes', 'avaliacaoRouter'],
+  //['conexoes', 'conexaoRouter'],
+  //['cupons', 'cupomRouter'],
+  //['estabelecimentos', 'estabelecimentoRouter'],
+  //['eventos', 'eventoRouter'],
+  //['favoritos', 'favoritoRouter'],
+  //['locais_visitados', 'localVisitadoRouter'],
+  //['organizacoes', 'organizacaoRouter'],
+  //['pagamentos', 'pagamentoRouter'],
   ['usuarios', 'usuarioRouter'],
-];
+]
 
 rotas.forEach(([path, file]) => {
-  const rota = require(`./routes/${file}`);
-  app.use(`/${path}`, rota);
-});
+  const rota = require(`./routes/${file}`)
+  app.use(`/${path}`, rota)
+})
 
-module.exports = app;
+module.exports = app
--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const app = require('./app');
+require('dotenv').config()
+const { app } = require('../app')
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}.`);
-});
+  console.log(`Servidor rodando na porta ${port}.`)
+})
