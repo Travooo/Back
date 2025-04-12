6d01f84cc590945ae4a98dff10728313fbf28813
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-12 15:51:57 -0300

    tests(usuario.test.js): otimizar estrutura

--- tests/usuario.test.js
+++ tests/usuario.test.js
@@ -22,9 +22,6 @@ describe('Testes de Integração - Usuario', () => {
     expect(res.status).toBe(201)
     expect(res.body).toHaveProperty('id')
     usuarioCriadoId = res.body.id
-    console.log('Resposta da criação:', res.body)
-    console.log('Usuário buscado:', res.body)
-    console.log('Total de usuários:', res.body.length)
   })
 
   test('Deve listar um usuário do Supabase', async () => {
@@ -34,40 +31,29 @@ describe('Testes de Integração - Usuario', () => {
     expect(res.body).toBeDefined()
     expect(res.body).toHaveProperty('id', usuarioCriadoId)
     expect(res.body).not.toHaveProperty('senha')
-    console.log('Resposta da criação:', res.body)
-    console.log('Usuário buscado:', res.body)
-    console.log('Total de usuários:', res.body.length)
   })
 
   test('Deve listar usuários do Supabase', async () => {
     const res = await request(app).get('/usuarios')
     console.log('Total de usuários:', res.body.length)
+    console.log('Resposta da listagem:', res.body)
     expect(res.status).toBe(200)
     expect(Array.isArray(res.body)).toBe(true)
     expect(res.body.length).toBeGreaterThan(0)
-    console.log('Resposta da criação:', res.body)
-    console.log('Usuário buscado:', res.body)
-    console.log('Total de usuários:', res.body.length)
   })
 
   test('Deve atualizar um usuario existente', async () => {
     const updates = { nome_usuario: 'NomeAtualizadoViaTeste' }
     const res = await request(app).put(`/usuarios/${usuarioCriadoId}`).send(updates)
-    console.log('Usuário atualizado:', res.body)
+    console.log('Resposta da atualização:', res.body)
     expect(res.status).toBe(200)
     expect(Array.isArray(res.body)).toBe(true)
     expect(res.body[0].nome_usuario).toBe('NomeAtualizadoViaTeste')
-    console.log('Resposta da criação:', res.body)
-    console.log('Usuário buscado:', res.body)
-    console.log('Total de usuários:', res.body.length)
   })
 
   test('Deve remover um usuário do Supabase', async () => {
     const res = await request(app).delete(`/usuarios/${usuarioCriadoId}`)
     console.log('Resposta da exclusão:', res.body)
     expect(res.status).toBe(204)
-    console.log('Resposta da criação:', res.body)
-    console.log('Usuário buscado:', res.body)
-    console.log('Total de usuários:', res.body.length)
   })
 })
