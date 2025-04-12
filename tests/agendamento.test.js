48959496c357184ce01741dc2e83f16023893eae
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-12 15:56:49 -0300

    feat: adicionar  agendamentoRouter.js que ficou de fora do commit anterior

--- tests/agendamento.test.js
+++ tests/agendamento.test.js
@@ -1,16 +1,17 @@
+require('dotenv').config()
+const app = require('../src/app')
 const request = require('supertest')
-const { app } = require('../app')
 
 let agendamentoCriadoId
 
-describe('Testes de Integração - Agendamento', () => {
+describe('Testes de Integração - Usuario', () => {
+  const new_agendamento = {
+    estabelecimento_id: 3,
+    usuario_id: 1,
+    horario: '2025-04-11T14:30',
+    detalhes: 'Usuário Teste API',
+  }
   test('Deve criar um agendamento via API', async () => {
-    const new_agendamento = {
-      estabelecimento_id: '3',
-      usuario_id: '1',
-      horario: '2025-04-06T14:30:00Z',
-      detalhes: 'Usuário Teste API',
-    }
     const res = await request(app).post('/agendamentos').send(new_agendamento)
     console.log('Resposta da criação:', res.body)
     expect(res.status).toBe(201)
@@ -24,29 +25,30 @@ describe('Testes de Integração - Agendamento', () => {
     expect(res.status).toBe(200)
     expect(res.body).toBeDefined()
     expect(res.body).toHaveProperty('id', agendamentoCriadoId)
+    expect(res.body).not.toHaveProperty('senha')
   })
 
   test('Deve listar agendamentos do Supabase', async () => {
-    const res = await request(app).get('/agendamentos')
+    const res = await request(app).get(`/agendamentos`)
     console.log('Total de agendamentos:', res.body.length)
+    console.log('Resposta da listagem:', res.body)
     expect(res.status).toBe(200)
     expect(Array.isArray(res.body)).toBe(true)
     expect(res.body.length).toBeGreaterThan(0)
   })
 
-  test('Deve atualizar um agendamento existente', async () => {
-    const updates = { horario: '2025-04-06T15:40:00Z' }
+  test('Deve atualizar o horario de um agendamento existente', async () => {
+    const updates = { horario: '2025-04-11T15:30' }
     const res = await request(app).put(`/agendamentos/${agendamentoCriadoId}`).send(updates)
-    console.log('Agendamento atualizado:', res.body)
+    console.log('Resposta da atualização:', res.body)
     expect(res.status).toBe(200)
     expect(Array.isArray(res.body)).toBe(true)
-    expect(res.body[0].horario.startsWith('2025-04-06T15:40:00')).toBe(true)
+    expect(res.body[0].horario).toBe('2025-04-11T18:30:00+00:00') // O banco converte para UTC
   })
 
   test('Deve remover um agendamento do Supabase', async () => {
     const res = await request(app).delete(`/agendamentos/${agendamentoCriadoId}`)
     console.log('Resposta da exclusão:', res.body)
     expect(res.status).toBe(204)
-    expect(res.body).not.toBeInstanceOf(Error)
   })
 })
