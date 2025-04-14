30a2ae7d77dbd38e36265a5fa1712c9ce78fb545
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-14 17:50:31 -0300

    feat: implementar CRUD de estabelecimentos (model, service, controller, router, testes)

--- src/models/Estabelecimento.js
+++ src/models/Estabelecimento.js
@@ -1,41 +1,111 @@
-class Estabelecimento {
-  constructor(organizacao_id, nome, sobre, endereco, foto_local, horarios) {
-    if (
-      !organizao_id ||
-      !nome ||
-      !sobre ||
-      !endereco ||
-      !foto_local ||
-      !horarios
-    ) {
-      throw new Error("Campos obrigatórios ausentes ou inválidos.");
-    }
-    if (!Number.isInteger(organizacao_id) || organizacao_id <= 0) {
-      throw new Error("Atributo 'organizao_id' inválido.");
-    }
-    if (typeof nome !== "string") {
-      throw new Error("Atributo 'nome' inválido.");
-    }
-    if (typeof sobre !== "string") {
-      throw new Error("Atributo 'sobre' inválido.");
-    }
-    if (typeof endereco !== "string") {
-      throw new Error("Atributo 'sobre' inválido.");
-    }
-    if (!(foto_local instanceof Buffer)) {
-      throw new Error("Atributo 'foto_local' inválido.");
-    }
-    if (typeof horarios !== "string") {
-      throw new Error("Atributo 'horarios' inválido.");
-    }
+const { validateString, validateFoto, validateNumber } = require('../utils/validators')
 
-    this.organizacao_id = organizacao_id;
-    this.nome = nome;
-    this.sobre = sobre;
-    this.endereco = endereco;
-    this.foto_local = foto_local;
-    this.horarios = horarios;
+const regexNome = /^[\p{L}0-9 _\-.',&()@]+$/u
+
+class Estabelecimento {
+  static #schema = {
+    // #: Privado
+    usuario_organizacao_id: {
+      tipo: 'number',
+      required: true,
+      atributo: 'usuario_organizacao_id',
+    },
+    nome: {
+      tipo: 'string',
+      required: true,
+      atributo: 'nome',
+      min: 4,
+      max: 30,
+      formato: regexNome,
+      erro: 'O nome do estabelecimento pode conter apenas letras, números e underline.',
+    },
+    sobre: {
+      tipo: 'string',
+      required: true,
+      atributo: 'sobre',
+      min: 10,
+      max: 300,
+    },
+    endereco: {
+      tipo: 'string',
+      required: true,
+      atributo: 'sobre',
+      min: 10,
+      max: 150,
+    },
+    horarios: {
+      tipo: 'string',
+      required: true,
+      atributo: 'horarios',
+      min: 10,
+      max: 100,
+    },
+    foto: {
+      tipo: 'foto',
+      required: true,
+      atributo: 'foto_perfil',
+    },
+    tipo: {
+      tipo: 'string',
+      required: true,
+      atributo: 'tipo',
+      min: 1,
+      max: 20,
+    },
+  }
+  constructor(data = {}) {
+    for (const key in Estabelecimento.#schema) {
+      const rule = Estabelecimento.#schema[key]
+      const valor = data[key]
+      if (valor === undefined || valor === null || valor === '') {
+        if (rule.required) {
+          throw new Error(`Atributo obrigatório '${key}' ausente.`)
+        }
+        this[key] = null
+      } else {
+        this[key] = Estabelecimento.#validate(valor, key)
+      }
+    }
+  }
+
+  static #validate(value, key) {
+    const rule = this.#schema[key]
+    if (!rule) return null
+    const { tipo, atributo = key, erro, ...rest } = rule
+    switch (tipo) {
+      case 'number':
+        return validateNumber(value, atributo)
+      case 'string':
+        return validateString(value, { atributo, erro_formato: erro, ...rest })
+      case 'foto':
+        return validateFoto(value, atributo)
+      default:
+        throw new Error(`Tipo de validação '${tipo}' não reconhecido para '${key}'`)
+    }
+  }
+
+  // Método auxiliar (pode ser exportado para update/create)
+  static validateBySchema(data = {}) {
+    const validados = {}
+    for (const key in data) {
+      if (key in this.#schema) {
+        validados[key] = this.#validate(data[key], key)
+      }
+    }
+    return validados
+  }
+
+  static getValidKeys() {
+    return Object.keys(this.#schema)
+  }
+
+  toJSON() {
+    const json = {}
+    for (const key of Estabelecimento.getValidKeys()) {
+      json[key] = this[key]
+    }
+    return json
   }
 }
 
-module.exports = Estabelecimento;
+module.exports = Estabelecimento
--- src/routes/estabelecimentoRouter.js
+++ src/routes/estabelecimentoRouter.js
@@ -1,11 +1,11 @@
-const EstabelecimentoController = require("../controllers/EstabelecimentoController");
-const express = require("express");
-const estabelecimentoRouter = express.Router();
+const EstabelecimentoController = require('../controllers/EstabelecimentoController')
+const express = require('express')
+const estabelecimentoRouter = express.Router()
 
-estabelecimentoRouter.post("/", EstabelecimentoController.create);
-estabelecimentoRouter.get("/:id", EstabelecimentoController.get_by_id);
-estabelecimentoRouter.get("/", EstabelecimentoController.get_all);
-estabelecimentoRouter.put("/:id", EstabelecimentoController.update);
-estabelecimentoRouter.delete("/:id", EstabelecimentoController.delete);
+estabelecimentoRouter.post('/', EstabelecimentoController.create)
+estabelecimentoRouter.get('/:id', EstabelecimentoController.getById)
+estabelecimentoRouter.get('/', EstabelecimentoController.getAll)
+estabelecimentoRouter.put('/:id', EstabelecimentoController.update)
+estabelecimentoRouter.delete('/:id', EstabelecimentoController.delete)
 
-module.exports = estabelecimentoRouter;
+module.exports = estabelecimentoRouter
