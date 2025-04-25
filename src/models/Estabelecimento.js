16e0035190fc280e67610c859523762163902194
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-25 18:38:03 -0300

    refactor: renomear e ajustar Estabelecimento.js usando ModeloBase.js

--- src/models/Estabelecimento.js
+++ src/models/Estabelecimento.js
@@ -1,111 +1,63 @@
-const { validateString, validateFoto, validateNumber } = require('../utils/validators')
+const ModeloBase = require("./ModeloBase");
 
-const regexNome = /^[\p{L}0-9 _\-.',&()@]+$/u
+const regexNome = /^[\p{L}0-9 _\-.',&()@]+$/u;
 
-class Estabelecimento {
+class Estabelecimento extends ModeloBase {
   static #schema = {
-    // #: Privado
     usuario_organizacao_id: {
-      tipo: 'number',
+      tipo: "number",
       required: true,
-      atributo: 'usuario_organizacao_id',
+      atributo: "usuario_organizacao_id",
     },
     nome: {
-      tipo: 'string',
+      tipo: "string",
       required: true,
-      atributo: 'nome',
+      atributo: "nome",
       min: 4,
       max: 30,
       formato: regexNome,
-      erro: 'O nome do estabelecimento pode conter apenas letras, números e underline.',
+      erro: "O nome do estabelecimento pode conter apenas letras, números e underline.",
     },
     sobre: {
-      tipo: 'string',
+      tipo: "string",
       required: true,
-      atributo: 'sobre',
+      atributo: "sobre",
       min: 10,
       max: 300,
     },
     endereco: {
-      tipo: 'string',
+      tipo: "string",
       required: true,
-      atributo: 'sobre',
+      atributo: "endereco",
       min: 10,
       max: 150,
     },
     horarios: {
-      tipo: 'string',
+      tipo: "string",
       required: true,
-      atributo: 'horarios',
+      atributo: "horarios",
       min: 10,
       max: 100,
     },
     foto: {
-      tipo: 'foto',
+      tipo: "foto",
       required: true,
-      atributo: 'foto_perfil',
+      atributo: "foto_perfil",
+      tiposPermitidos: ["image/jpeg", "image/png"],
+      tamanhoMaxMB: 3,
     },
     tipo: {
-      tipo: 'string',
+      tipo: "string",
       required: true,
-      atributo: 'tipo',
+      atributo: "tipo",
       min: 1,
       max: 20,
     },
-  }
-  constructor(data = {}) {
-    for (const key in Estabelecimento.#schema) {
-      const rule = Estabelecimento.#schema[key]
-      const valor = data[key]
-      if (valor === undefined || valor === null || valor === '') {
-        if (rule.required) {
-          throw new Error(`Atributo obrigatório '${key}' ausente.`)
-        }
-        this[key] = null
-      } else {
-        this[key] = Estabelecimento.#validate(valor, key)
-      }
-    }
-  }
+  };
 
-  static #validate(value, key) {
-    const rule = this.#schema[key]
-    if (!rule) return null
-    const { tipo, atributo = key, erro, ...rest } = rule
-    switch (tipo) {
-      case 'number':
-        return validateNumber(value, atributo)
-      case 'string':
-        return validateString(value, { atributo, erro_formato: erro, ...rest })
-      case 'foto':
-        return validateFoto(value, atributo)
-      default:
-        throw new Error(`Tipo de validação '${tipo}' não reconhecido para '${key}'`)
-    }
-  }
-
-  // Método auxiliar (pode ser exportado para update/create)
-  static validateBySchema(data = {}) {
-    const validados = {}
-    for (const key in data) {
-      if (key in this.#schema) {
-        validados[key] = this.#validate(data[key], key)
-      }
-    }
-    return validados
-  }
-
-  static getValidKeys() {
-    return Object.keys(this.#schema)
-  }
-
-  toJSON() {
-    const json = {}
-    for (const key of Estabelecimento.getValidKeys()) {
-      json[key] = this[key]
-    }
-    return json
+  static getSchema() {
+    return this.#schema;
   }
 }
 
-module.exports = Estabelecimento
+module.exports = Estabelecimento;
