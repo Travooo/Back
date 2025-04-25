8df4e39a65c29bb00b81d1b26f32e1847d6bdf70
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-25 20:08:31 -0300

    refactor: renomear e ajustar Avaliacao.js usando ModeloBase.js

--- src/models/Avaliacao.js
+++ src/models/Avaliacao.js
@@ -1,84 +1,38 @@
-const { validateNumber, validateString, validateDate } = require('../utils/validators')
+const ModeloBase = require("./ModeloBase");
 
-class Avaliacao {
+class Avaliacao extends ModeloBase {
   static #schema = {
-    // #: Privado
     estabelecimento_id: {
-      tipo: 'number',
+      tipo: "number",
       required: true,
-      atributo: 'estabelecimento_id',
+      atributo: "estabelecimento_id",
     },
     usuario_id: {
-      tipo: 'number',
+      tipo: "number",
       required: true,
-      atributo: 'usuario_id',
+      atributo: "usuario_id",
     },
     comentario: {
-      tipo: 'string',
+      tipo: "string",
       required: true,
-      atributo: 'comentario',
+      atributo: "comentario",
       min: 4,
       max: 100,
     },
     numero_estrelas: {
-      tipo: 'number',
+      tipo: "number",
       required: true,
-      atributo: 'numero_estrelas',
+      atributo: "numero_estrelas",
     },
     data_comentario: {
-      tipo: 'date',
-      atributo: 'data_comentario',
+      tipo: "date",
+      required: true,
+      atributo: "data_comentario",
     },
-  }
-  constructor(data = {}) {
-    for (const key in Avaliacao.#schema) {
-      const rule = Avaliacao.#schema[key]
-      const valor = data[key]
-      if (valor === undefined || valor === null || valor === '') {
-        if (rule.required) {
-          throw new Error(`Atributo obrigatório '${key}' ausente.`)
-        }
-        this[key] = null
-      } else {
-        this[key] = Avaliacao.#validate(valor, key)
-      }
-    }
-  }
-  static #validate(value, key) {
-    const rule = this.#schema[key]
-    if (!rule) return null
-    const { tipo, atributo = key, erro, ...rest } = rule
-    switch (tipo) {
-      case 'string':
-        return validateString(value, { atributo, erro_formato: erro, ...rest })
-      case 'number':
-        return validateNumber(value, atributo)
-      case 'date':
-        return validateDate(value, atributo)
-      default:
-        throw new Error(`Tipo de validação '${tipo}' não reconhecido para '${key}'`)
-    }
-  }
-  // Método auxiliar (pode ser exportado para update/create)
-  static validateBySchema(data = {}) {
-    const validados = {}
-    for (const key in data) {
-      if (key in this.#schema) {
-        validados[key] = this.#validate(data[key], key)
-      }
-    }
-    return validados
-  }
-  static getValidKeys() {
-    return Object.keys(this.#schema)
-  }
-  toJSON() {
-    const json = {}
-    for (const key of Avaliacao.getValidKeys()) {
-      json[key] = this[key]
-    }
-    return json
+  };
+  static getSchema() {
+    return this.#schema;
   }
 }
 
-module.exports = Avaliacao
+module.exports = Avaliacao;
