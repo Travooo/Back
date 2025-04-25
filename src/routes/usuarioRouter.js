c670a90cd4ca1f1287a722de3c6d2d789213c71f
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-25 13:17:32 -0300

    refactor(usuario): incluir usuarioRouter não incluso em commit anterior

--- src/routes/usuarioRouter.js
+++ src/routes/usuarioRouter.js
@@ -1,11 +1,12 @@
-const UsuarioController = require('../controllers/UsuarioController');
-const express = require('express');
+const express = require("express");
+const UsuarioController = require("../controllers/UsuarioController");
 const usuarioRouter = express.Router();
 
-usuarioRouter.post('/', UsuarioController.create);
-usuarioRouter.get('/', UsuarioController.getAll);
-usuarioRouter.get('/:id', UsuarioController.getById);
-usuarioRouter.put('/:id', UsuarioController.update);
-usuarioRouter.delete('/:id', UsuarioController.delete);
+usuarioRouter.post("/", UsuarioController.create);
+usuarioRouter.get("/", UsuarioController.getAll);
+usuarioRouter.get("/:id", UsuarioController.getById);
+usuarioRouter.get("/:email", UsuarioController.getByEmail);
+usuarioRouter.put("/:id", UsuarioController.update);
+usuarioRouter.delete("/:id", UsuarioController.delete);
 
 module.exports = usuarioRouter;
