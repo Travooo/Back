92fbe476655cdb7462c927597f1d3a4f9f6171f8
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-25 18:37:33 -0300

    refactor: renomear usuarioOrganizaoRouter.js e atualizar chamadas de métodos

--- src/routes/estabelecimentoRouter.js
+++ src/routes/estabelecimentoRouter.js
@@ -1,11 +1,11 @@
-const EstabelecimentoController = require('../controllers/EstabelecimentoController')
-const express = require('express')
-const estabelecimentoRouter = express.Router()
+const express = require("express");
+const EstabelecimentoController = require("../controllers/EstabelecimentoController");
+const estabelecimentoRouter = express.Router();
 
-estabelecimentoRouter.post('/', EstabelecimentoController.create)
-estabelecimentoRouter.get('/:id', EstabelecimentoController.getById)
-estabelecimentoRouter.get('/', EstabelecimentoController.getAll)
-estabelecimentoRouter.put('/:id', EstabelecimentoController.update)
-estabelecimentoRouter.delete('/:id', EstabelecimentoController.delete)
+estabelecimentoRouter.post("/", EstabelecimentoController.create);
+estabelecimentoRouter.get("/:id", EstabelecimentoController.getById);
+estabelecimentoRouter.get("/", EstabelecimentoController.getAll);
+estabelecimentoRouter.put("/:id", EstabelecimentoController.update);
+estabelecimentoRouter.delete("/:id", EstabelecimentoController.delete);
 
-module.exports = estabelecimentoRouter
+module.exports = estabelecimentoRouter;
