f687c7f55fd7bdf9c3f8b02a0711227b6fa1c92a
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-25 18:39:05 -0300

    refactor: otimizar EstabelecimentoService.js

--- src/services/EstabelecimentoService.js
+++ src/services/EstabelecimentoService.js
@@ -1,73 +1,91 @@
-const { validateNumber, getIfExists } = require('../utils/validators')
-const Estabelecimento = require('../models/Estabelecimento')
-const supabase = require('../config/supabaseClient')
+const Estabelecimento = require("../models/Estabelecimento");
+const supabase = require("../config/supabaseClient");
+const { validateNumber, cleanObject } = require("../utils/validators");
+const UsuarioOrganizacaoService = require("./UsuarioOrganizacaoService");
 
 class EstabelecimentoService {
-  static async create(estabelecimento_data) {
-    try {
-      const estabelecimento = new Estabelecimento(estabelecimento_data)
-
-      const organizacaoExiste = await getIfExists({
-        tabela: 'usuarios_organizacao',
-        coluna: 'id',
-        value: estabelecimento.usuario_organizacao_id,
-      }).catch(() => null)
-      if (!organizacaoExiste) {
-        throw new Error(`Usuário organização #${estabelecimento.usuario_organizacao_id} não localizado no Supabase.`)
-      }
-
-      const { data, error } = await supabase.from('estabelecimentos').insert(estabelecimento.toJSON()).single().select()
-      if (error) {
-        throw new Error(error.message)
-      }
-      return data
-    } catch (error) {
-      throw new Error(error.message)
+  static async create(estabelecimento) {
+    if (!estabelecimento || typeof estabelecimento !== "object") {
+      throw new Error("Dados inválidos ou não fornecidos.");
     }
+    const validated = Estabelecimento.validateBySchema(estabelecimento);
+    // Verifica se já existe estabelecimento com 'usuario_organizacao_id'
+    const organizacao = await UsuarioOrganizacaoService.getById(
+      validated.usuario_organizacao_id
+    );
+    if (!organizacao) {
+      throw new Error(
+        `Usuário organização #${validated.usuario_organizacao_id} não encontrado.`
+      );
+    }
+    const { data, error } = await supabase
+      .from("estabelecimentos")
+      .insert(validated)
+      .select()
+      .maybeSingle();
+    if (!data) throw new Error("Resposta do banco de dados não retornada.");
+    if (error) throw new Error(error.message);
+    return data;
   }
 
   static async getById(id) {
-    const estabelecimentoId = validateNumber(id, 'estabelecimento_id')
-    const { data, error } = await supabase.from('estabelecimentos').select('*').eq('id', estabelecimentoId).single()
-    if (error) return Error(error.message)
-    return data
+    const validId = validateNumber(id, "estabelecimento_id");
+    const { data, error } = await supabase
+      .from("estabelecimentos")
+      .select("*")
+      .eq("id", validId)
+      .maybeSingle();
+    if (error) throw error;
+    return data;
   }
 
   static async getAll() {
-    const { data, error } = await supabase.from('estabelecimentos').select('*')
-    if (error) throw new Error(error.message)
-    return data
+    const { data, error } = await supabase.from("estabelecimentos").select("*");
+    if (error) throw error;
+    if (!data) throw new Error("Resposta do banco de dados não retornada.");
+    return data;
   }
 
   static async update(id, updates) {
-    const estabelecimentoId = validateNumber(id, 'estabelecimento_id')
-    if (!updates || typeof updates !== 'object') {
-      throw new Error('Atualizações inválidas ou não fornecidas.')
+    if (!updates || typeof updates !== "object") {
+      throw new Error("Atualizações inválidas ou não fornecidas.");
     }
-
-    const validados = {}
-    for (const key of Object.keys(updates)) {
-      if (!Estabelecimento.getValidKeys().includes(key)) continue
-      validados[key] = Estabelecimento.validateBySchema({ [key]: updates[key] })[key]
-    }
-
+    const validId = validateNumber(id, "estabelecimento_id");
+    // Verifica se o estabelecimento existe antes de atualizar
+    const existente = await this.getById(validId);
+    if (!existente) throw new Error("Estabelecimento não encontrado.");
+    // Valida os dados conforme schema
+    const camposValidos = cleanObject(updates);
+    const validados = Estabelecimento.validateBySchema(camposValidos);
+    // Inserção no banco
     const { data, error } = await supabase
-      .from('estabelecimentos')
+      .from("estabelecimentos")
       .update(validados)
-      .eq('id', estabelecimentoId)
+      .eq("id", validId)
       .select()
-    if (error) {
-      throw new Error(error.message)
-    }
-    return data
+      .single();
+    if (error) throw error;
+    if (!data) throw new Error("Resposta do banco de dados não retornada.");
+    return data;
   }
 
   static async delete(id) {
-    const estabelecimentoId = validateNumber(id, 'estabelecimento_id')
-    const { data, error } = await supabase.from('estabelecimentos').delete().eq('id', estabelecimentoId)
-    if (error) return Error(error.message)
-    return data
+    // Verifica se o registro existe antes de atualizar
+    const estabelecimento = await EstabelecimentoService.getById(
+      validateNumber(id, "estabelecimento_id")
+    );
+    if (!estabelecimento) {
+      throw new Error("Estabeleciment não encontrado.");
+    }
+    const { data, error } = await supabase
+      .from("estabelecimentos")
+      .delete()
+      .eq("id", id)
+      .select()
+      .single();
+    if (error) throw error;
+    return data;
   }
 }
 
-module.exports = EstabelecimentoService
+module.exports = EstabelecimentoService;
