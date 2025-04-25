7c7cc05741d808e81178589747c25ee775a5b353
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-25 17:11:10 -0300

    refactor: ajuste em LocalVisitado.js usando ModeloBase.js

--- src/models/LocalVisitado.js
+++ src/models/LocalVisitado.js
@@ -1,71 +1,30 @@
-const { validateNumber, validateDate } = require('../utils/validators')
+const ModeloBase = require("./ModeloBase");
 
-class LocalVisitado {
-  static #schema = {
-    // #: Privado
+class LocalVisitado extends ModeloBase {
+  static schema = {
     estabelecimento_id: {
-      tipo: 'number',
+      tipo: "number",
       required: true,
-      atributo: 'estabelecimento_id',
+      atributo: "estabelecimento_id",
+      erro: "'estabelecimento_id' deve ser um número válido.",
     },
     usuario_id: {
-      tipo: 'number',
+      tipo: "number",
       required: true,
-      atributo: 'usuario_id',
+      atributo: "usuario_id",
+      erro: "'usuario_id' deve ser um número válido.",
     },
     data_visita: {
-      tipo: 'date',
+      tipo: "date",
       required: true,
-      atributo: 'data_nascimento',
+      atributo: "data_visita",
+      erro: "'data_visita' deve ser uma data válida.",
     },
-  }
-  constructor(data = {}) {
-    for (const key in LocalVisitado.#schema) {
-      const rule = LocalVisitado.#schema[key]
-      const valor = data[key]
-      if (valor === undefined || valor === null || valor === '') {
-        if (rule.required) {
-          throw new Error(`Atributo obrigatório '${key}' ausente.`)
-        }
-        this[key] = null
-      } else {
-        this[key] = LocalVisitado.#validate(valor, key)
-      }
-    }
-  }
-  static #validate(value, key) {
-    const rule = this.#schema[key]
-    if (!rule) return null
-    const { tipo, atributo = key, erro, ...rest } = rule
-    switch (tipo) {
-      case 'number':
-        return validateNumber(value, atributo)
-      case 'date':
-        return validateDate(value, atributo)
-      default:
-        throw new Error(`Tipo de validação '${tipo}' não reconhecido para '${key}'`)
-    }
-  }
-  // Método auxiliar (pode ser exportado para outras camadas)
-  static validateBySchema(data = {}) {
-    const validados = {}
-    for (const key in data) {
-      if (key in this.#schema) {
-        validados[key] = this.#validate(data[key], key)
-      }
-    }
-    return validados
-  }
-  static getValidKeys() {
-    return Object.keys(this.#schema)
-  }
-  toJSON() {
-    const json = {}
-    for (const key of LocalVisitado.getValidKeys()) {
-      json[key] = this[key]
-    }
-    return json
+  };
+
+  static getSchema() {
+    return this.schema;
   }
 }
 
-module.exports = LocalVisitado
+module.exports = LocalVisitado;
