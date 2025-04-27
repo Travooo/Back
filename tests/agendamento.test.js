675b37ae3ca4f8e811ecf17c26149056869a4640
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-26 21:11:22 -0300

    tests: otimizar agendamento.test.js

--- tests/agendamento.test.js
+++ tests/agendamento.test.js
@@ -1,54 +1,62 @@
-require('dotenv').config()
-const app = require('../src/app')
-const request = require('supertest')
+require("dotenv").config();
+const app = require("../src/app");
+const request = require("supertest");
 
-let agendamentoCriadoId
+let agendamentoCriadoId;
 
-describe('Testes de Integração - Agendamento', () => {
-  const new_agendamento = {
-    estabelecimento_id: 3,
-    usuario_id: 1,
-    horario: '2025-04-11T14:30',
-    detalhes: 'Usuário Teste API',
-  }
+describe("Testes de Integração - Agendamento", () => {
+  test("Deve criar um agendamentos via API", async () => {
+    const res = await request(app).post("/agendamentos").send({
+      servico_id: 3,
+      usuario_id: 2,
+      horario: "2025-02-10T19:30",
+      detalhes: "Agendamento Teste API",
+    });
+    console.log("Resposta da criação:", res.body);
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty("id");
+    agendamentoCriadoId = res.body.id;
+  });
 
-  test('Deve criar um agendamento via API', async () => {
-    const res = await request(app).post('/agendamentos').send(new_agendamento)
-    console.log('Resposta da criação:', res.body)
-    expect(res.status).toBe(201)
-    expect(res.body).toHaveProperty('id')
-    agendamentoCriadoId = res.body.id
-  })
+  describe("Leitura", () => {
+    test("Deve listar o agendamento criado pelo ID", async () => {
+      const res = await request(app).get(
+        `/agendamentos/${agendamentoCriadoId}`
+      );
+      console.log("Resposta da listagem:", res.body);
+      expect(res.status).toBe(200);
+      expect(res.body).toBeDefined();
+      expect(res.body).toHaveProperty("id", agendamentoCriadoId);
+    });
+    test("Deve listar todos os agendamentos cadastrados", async () => {
+      const res = await request(app).get("/agendamentos");
+      console.log("Resposta da listagem:", res.body);
+      console.log("Total de agendamentos:", res.body.length);
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.body)).toBe(true);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
+  });
 
-  test('Deve listar um agendamento do Supabase', async () => {
-    const res = await request(app).get(`/agendamentos/${agendamentoCriadoId}`)
-    console.log('Resposta da listagem:', res.body)
-    expect(res.status).toBe(200)
-    expect(res.body).toBeDefined()
-    expect(res.body).toHaveProperty('id', agendamentoCriadoId)
-  })
+  describe("Atualização", () => {
+    test("Deve atualizar o horario de um agendamento existente", async () => {
+      const updates = { horario: "2025-02-11T18:30" };
+      const res = await request(app)
+        .put(`/agendamentos/${agendamentoCriadoId}`)
+        .send(updates);
+      console.log("Resposta da atualização:", res.body);
+      expect(res.status).toBe(200);
+      expect(res.body.horario).toContain("2025-02-11T18:30");
+    });
+  });
 
-  test('Deve listar agendamentos do Supabase', async () => {
-    const res = await request(app).get(`/agendamentos`)
-    console.log('Resposta da listagem:', res.body)
-    console.log('Total de agendamentos:', res.body.length)
-    expect(res.status).toBe(200)
-    expect(Array.isArray(res.body)).toBe(true)
-    expect(res.body.length).toBeGreaterThan(0)
-  })
-
-  test('Deve atualizar o horario de um agendamento existente', async () => {
-    const updates = { horario: '2025-04-11T15:30' }
-    const res = await request(app).put(`/agendamentos/${agendamentoCriadoId}`).send(updates)
-    console.log('Resposta da atualização:', res.body)
-    expect(res.status).toBe(200)
-    expect(Array.isArray(res.body)).toBe(true)
-    expect(res.body[0].horario).toBe('2025-04-11T18:30:00+00:00') // O banco converte para UTC
-  })
-
-  test('Deve remover um agendamento do Supabase', async () => {
-    const res = await request(app).delete(`/agendamentos/${agendamentoCriadoId}`)
-    console.log('Resposta da exclusão:', res.body)
-    expect(res.status).toBe(204)
-  })
-})
+  describe("Exclusão", () => {
+    test("Deve remover um agendamento do Supabase", async () => {
+      const res = await request(app).delete(
+        `/agendamentos/${agendamentoCriadoId}`
+      );
+      console.log("Resposta da exclusão:", res.body);
+      expect(res.status).toBe(200);
+    });
+  });
+});
