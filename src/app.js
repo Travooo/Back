1af8bc4a9413649a48fbba2538b22522e1710f81
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-26 21:46:06 -0300

    chore(app.js): atualizar nomes das rotas de acordo com supabase

--- src/app.js
+++ src/app.js
@@ -1,41 +1,49 @@
-const express = require('express')
-const cors = require('cors')
-const supabase = require('./config/supabaseClient')
+const express = require("express");
+const cors = require("cors");
+const supabase = require("./config/supabaseClient");
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 // Rota teste de conexão Supabase:
-app.get('/test-supabase', async (req, res) => {
+app.get("/test-supabase", async (req, res) => {
   try {
-    const { data, error } = await supabase.from('usuarios').select('*').limit(10)
-    if (error) throw error
+    const { data, error } = await supabase
+      .from("usuarios")
+      .select("*")
+      .limit(10);
+    if (error) throw error;
 
-    res.json({ success: true, data })
+    res.json({ success: true, data });
   } catch (error) {
-    res.status(500).json({ success: false, message: 'Erro ao conectar ao Supabase. ', error })
+    res.status(500).json({
+      success: false,
+      message: "Erro ao conectar ao Supabase. ",
+      error,
+    });
   }
-})
+});
 
 const rotas = [
-  ['agendamentos', 'agendamentoRouter'],
-  ['anexos', 'anexoRouter'],
-  ['avaliacoes', 'avaliacaoRouter'],
-  ['conexoes', 'conexaoRouter'],
-  ['cupons', 'cupomRouter'],
-  ['estabelecimentos', 'estabelecimentoRouter'],
-  ['eventos', 'eventoRouter'],
-  ['favoritos', 'favoritoRouter'],
-  ['locais_visitados', 'localVisitadoRouter'],
-  ['organizacoes', 'organizacaoRouter'],
-  ['pagamentos', 'pagamentoRouter'],
-  ['usuarios', 'usuarioRouter'],
-]
+  //["agendamentos", "agendamentoRouter"],
+  //['anexos', 'anexoRouter'],
+  //["avaliacoes", "avaliacaoRouter"],
+  //['conexoes', 'conexaoRouter'],
+  //['cupons', 'cupomRouter'],
+  //['eventos', 'eventoRouter'],
+  //['favoritos', 'favoritoRouter'],
+  //["locais_visitados", "localVisitadoRouter"],
+  //["notificacoes", "notificacaoRouter"],
+  //['pagamentos', 'pagamentoRouter'],
+  //["servicos", "servicoRouter"],
+  //["usuarios", "usuarioRouter"],
+  //["usuarios_organizacao", "usuarioOrganizacaoRouter"],
+];
 
 rotas.forEach(([path, file]) => {
-  const rota = require(`./routes/${file}`)
-  app.use(`/${path}`, rota)
-})
+  const rota = require(`./routes/${file}`);
+  app.use(`/${path}`, rota);
+});
 
-module.exports = app
+module.exports = app;
