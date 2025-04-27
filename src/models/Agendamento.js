bef7a1f39c8933d16a1af7f8cdc76b72daf56388
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-26 21:14:21 -0300

    refactor: reorganizar Agendamento.js conforme ModeloBase.js

--- src/models/Agendamento.js
+++ src/models/Agendamento.js
@@ -1,84 +1,32 @@
-const { validateNumber, validateString, validateDate } = require('../utils/validators')
+const ModeloBase = require("./ModeloBase");
 
-class Agendamento {
+class Agendamento extends ModeloBase {
   static #schema = {
-    // #: Privado
-    estabelecimento_id: {
-      tipo: 'number',
+    servico_id: {
+      tipo: "number",
       required: true,
-      atributo: 'estabelecimento_id',
+      atributo: "servico_id",
     },
     usuario_id: {
-      tipo: 'number',
+      tipo: "number",
       required: true,
-      atributo: 'usuario_id',
+      atributo: "usuario_id",
     },
     horario: {
-      tipo: 'date',
+      tipo: "date",
       required: true,
-      atributo: 'horario',
+      atributo: "horario",
     },
     detalhes: {
-      tipo: 'string',
-      atributo: 'detalhes',
+      tipo: "string",
+      atributo: "detalhes",
       min: 10,
       max: 300,
     },
-  }
-  constructor(data = {}) {
-    for (const key in Agendamento.#schema) {
-      const rule = Agendamento.#schema[key]
-      const valor = data[key]
-      if (valor === undefined || valor === null || valor === '') {
-        if (rule.required) {
-          throw new Error(`Atributo obrigatório '${key}' ausente.`)
-        }
-        this[key] = null
-      } else {
-        this[key] = Agendamento.#validate(valor, key)
-      }
-    }
-  }
-
-  static #validate(value, key) {
-    const rule = this.#schema[key]
-    if (!rule) return null
-
-    const { tipo, atributo = key, erro, ...rest } = rule
-
-    switch (tipo) {
-      case 'string':
-        return validateString(value, { atributo, erro_formato: erro, ...rest })
-      case 'number':
-        return validateNumber(value, atributo)
-      case 'date':
-        return validateDate(value, atributo)
-      default:
-        throw new Error(`Tipo de validação '${tipo}' não reconhecido para '${key}'`)
-    }
-  }
-  // Método auxiliar (pode ser exportado para update/create)
-  static validateBySchema(data = {}) {
-    const validados = {}
-    for (const key in data) {
-      if (key in this.#schema) {
-        validados[key] = this.#validate(data[key], key)
-      }
-    }
-    return validados
-  }
-
-  static getValidKeys() {
-    return Object.keys(this.#schema)
-  }
-
-  toJSON() {
-    const json = {}
-    for (const key of Agendamento.getValidKeys()) {
-      json[key] = this[key]
-    }
-    return json
+  };
+  static getSchema() {
+    return this.#schema;
   }
 }
 
-module.exports = Agendamento
+module.exports = Agendamento;
