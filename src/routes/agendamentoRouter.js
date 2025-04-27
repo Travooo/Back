afbb88dd998d7a4e2b7bbf9622d684a86bfc410c
Luiz Bastos <luizeduardobastoslima@gmail.com>
2025-04-26 21:09:46 -0300

    chore: reorganizar imports de agendamentoRouter.js

--- src/routes/agendamentoRouter.js
+++ src/routes/agendamentoRouter.js
@@ -1,11 +1,11 @@
-const AgendamentoController = require('../controllers/AgendamentoController')
-const express = require('express')
-const agendamentoRouter = express.Router()
+const express = require("express");
+const AgendamentoController = require("../controllers/AgendamentoController");
+const agendamentoRouter = express.Router();
 
-agendamentoRouter.post('/', AgendamentoController.create)
-agendamentoRouter.get('/', AgendamentoController.getAll)
-agendamentoRouter.get('/:id', AgendamentoController.getById)
-agendamentoRouter.put('/:id', AgendamentoController.update)
-agendamentoRouter.delete('/:id', AgendamentoController.delete)
+agendamentoRouter.post("/", AgendamentoController.create);
+agendamentoRouter.get("/", AgendamentoController.getAll);
+agendamentoRouter.get("/:id", AgendamentoController.getById);
+agendamentoRouter.put("/:id", AgendamentoController.update);
+agendamentoRouter.delete("/:id", AgendamentoController.delete);
 
-module.exports = agendamentoRouter
+module.exports = agendamentoRouter;
