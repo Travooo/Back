1b3571dfd33e7bb28ec3a867a464b0ab62fad6e4
Luiz <Luiz>
2025-05-26 19:46:32 -0300

    remover arquivos 'auth' obsoletos

--- routes/rota_auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controller/controllerAuth');
-
-router.post('/login', authController.login);
-router.post('/cadastro', authController.cadastrar);
-
-module.exports = router;
