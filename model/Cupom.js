2107d51b2e608c2e77f1d80b0be32da3948322a6
Mayara Medino <medino.dev@gmail.com>
2025-06-13 19:53:13 -0300

    Ajustes

--- model/Cupom.js
+++ model/Cupom.js
@@ -1,10 +1,10 @@
 class Cupom {
-    constructor({id, estabelecimento_id, descricao, created_at, expiration, nome}) {
+    constructor({id, estabelecimento_id, descricao, created_at, expiration, nome, organizacao_id}) {
         if (id !== undefined) {
             this.id = id;
           }
         this.estabelecimento_id = estabelecimento_id;
-        this.organizacao_id = organizacao_id;  
+        this.organizacao_id = organizacao_id;
         this.descricao = descricao;
         this.created_at = created_at;
         this.expiration = expiration;
